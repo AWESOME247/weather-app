874013f47aaf2ee147e1815be476992405df5e63
AWESOME247 <omoye6996@gmail.com>
2021-03-21 18:49:56 +0100

    update css

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,11 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const container = document.querySelector('.container')
-const container2 = document.querySelector('.container2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value;
     container.textContent = 'Loading...';
-    container2.textContent = '';
 
     fetch(`/weather?address=${location}`).then((res) => {
         res.json().then(data => {
@@ -15,8 +13,10 @@ weatherForm.addEventListener('submit', (e) => {
             else {
                 container.textContent = data.Location;
                 container.style.border = '1px solid #C8C9C8';
-                container2.textContent = data.Forcastdata;
-                container2.style.border = '1px solid #C8C9C8';
+
+                container.textContent = data.Forcastdata;
+                container.style.border = '1px solid #C8C9C8';
+
             }
         })
     });
